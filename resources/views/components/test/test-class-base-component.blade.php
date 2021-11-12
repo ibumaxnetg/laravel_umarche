3169c95293ce3fb6b1e26b51e653d22eda9e3826
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-12 17:25:55 +0900

    クラスベースコンポーネント

---
+++ resources/views/components/test/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMassage }}</div>
+    <div>{{ $defaultMessage }}</div>
+</div>
--- resources/views/test/component-test2.blade.php
+++ resources/views/test/component-test2.blade.php
@@ -2,5 +2,7 @@
   <x-slot name="header">へっだー２</x-slot>
   component-test2.blade.php<br>
   にめーぺーじだぜい
+    <div><x-test-class-base classBaseMassage="くらすべーすメッセージです" /></div>
+    <div><x-test-class-base classBaseMassage="くらすべーすメッセージです" defaultMessage="初期値から変更しています" /></div>
   </x-test.app>
   
