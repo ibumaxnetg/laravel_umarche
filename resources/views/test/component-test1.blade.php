559be145e400b318dcf844a6f0bd61b9e7d464ab
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-12 16:34:12 +0900

    コンポー人との属性のマージ

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -2,5 +2,6 @@
   <x-slot name="header">へっだー1</x-slot>
 component-test1.blade.php
 だぜい
-<x-test.card title="たいとる" content="こんてんつ" :message="$message" />
+  <x-test.card title="たいとる" content="こんてんつ" :message="$message" />
+  <x-test.card title="たいとる" class="bg-red-300" />
 </x-test.app>
