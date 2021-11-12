7652340a7b8fbd3a84953f4f6d727a46acc77eca
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-12 15:18:18 +0900

    sec02_componentSlotWithName

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -1,4 +1,5 @@
 <x-test.app>
+  <x-slot name="header">へっだー1</x-slot>
 component-test1.blade.php
 だぜい
 </x-test.app>
--- resources/views/test/component-test2.blade.php
+++ resources/views/test/component-test2.blade.php
@@ -1,4 +1,5 @@
 <x-test.app>
+  <x-slot name="header">へっだー２</x-slot>
   component-test2.blade.php<br>
   にめーぺーじだぜい
   </x-test.app>
