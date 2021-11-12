254f3115040e1605d64fd5aafdaeba4f353bb615
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-12 15:07:28 +0900

    sec02_componentSlot

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -1,1 +1,4 @@
+<x-test.app>
 component-test1.blade.php
+だぜい
+</x-test.app>
--- resources/views/test/component-test2.blade.php
+++ resources/views/test/component-test2.blade.php
@@ -1,1 +1,5 @@
-component-test2.blade.php
+<x-test.app>
+  component-test2.blade.php<br>
+  にめーぺーじだぜい
+  </x-test.app>
+  
