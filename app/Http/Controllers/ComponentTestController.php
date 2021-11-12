b01a91ba42b94fbd131890d3c72ef8aca74a2c70
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-12 15:51:56 +0900

    コンポーネントで変数の読み込み

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -8,7 +8,9 @@ class ComponentTestController extends Controller
 {
     //
     public function showComponent1() {
-        return view('test.component-test1');
+        $message = 'メッセージ';
+
+        return view('test.component-test1', compact('message'));
     }
 
     public function showComponent2() {
--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -2,5 +2,5 @@
   <x-slot name="header">へっだー1</x-slot>
 component-test1.blade.php
 だぜい
-<x-test.card title="たいとる" content="こんてんつ" />
+<x-test.card title="たいとる" content="こんてんつ" :message="$message" />
 </x-test.app>
