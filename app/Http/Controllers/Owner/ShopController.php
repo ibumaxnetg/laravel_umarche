aedaa07ec2ef267ad1bbcf97e9027e882336cd26
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-25 14:50:46 +0900

    フォームリクエスト　バリデーションルール

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -9,6 +9,7 @@ use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class ShopController extends Controller
 {
@@ -51,7 +52,7 @@ class ShopController extends Controller
         return view('owner.shops.edit', compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image; // ファイルを作成し一時保存 
         if(!is_null($imageFile) && $imageFile->isValid() ){ // 画像が null でない && いｓValid() 一時保存完了か
