9a4aaf3b65a775c19521f6653d2452aa9b39dfaf
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-25 13:18:55 +0900

    index画面

--- resources/views/owner/shops/index.blade.php
+++ resources/views/owner/shops/index.blade.php
@@ -10,7 +10,27 @@
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200">
                 @foreach ( $shops as $shop)
-                    {{ $shop->name }}
+                <div class="w-1/2 p-4">
+                    <a href="{{ route('owner.shops.edit', ['shop' => $shop->id ]) }}">
+                        <div class="border rounded-md p4">
+                            <div class="mb-4">
+                                @if ($shop->is_selling)
+                                <span class="border rounded-md p4 bg-blue-400 text-white">販売中</span>
+                                @else
+                                <span class="border rounded-md p4 bg-red-400 text-white">販売停止中</span>
+                                @endif
+                                <div class="text-xl">{{ $shop->name }}</div>
+                                <div>
+                                @if (empty($shop->filename))
+                                    <img src="{{ asset('images/no_image.jpg') }}" />
+                                @else
+                                    <img src="{{ asset('storage/shops/'.$shop->filename ) }}" />
+                                @endif
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
                 @endforeach
                 <p><a href="/component-test1">てすと１</a></p>
                 <p><a href="/component-test2">てすと2</a></p>
