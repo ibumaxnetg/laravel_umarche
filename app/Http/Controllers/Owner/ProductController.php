015dd8a4d9681a0cf4ce6bb83beea33519d481e6
ibumaxnetg <ibumaxnet@gmail.com>
2021-11-29 13:36:05 +0900

    product index, eager_loading

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -5,8 +5,37 @@ namespace App\Http\Controllers\Owner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\Owner;
+use App\Models\Image;
+use App\Models\SecondaryCategory;
+use App\Models\Product;
+
+
+
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        // ミドルウェアの設定
+        $this->middleware(function($request, $next){
+            $id = $request->route()->parameter('product'); //shopのid取得
+            if(!is_null($id)){ // null判定
+                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+                $productsId = (int)$productsOwnerId; // キャスト 文字列→数値に型変換 
+
+                if($productsId !== Auth::id()){ // 同じでなかったら
+                    abort(404); // 404画面表示 
+                }
+            }
+            return $next($request);
+        });
+             
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,6 +44,20 @@ class ProductController extends Controller
     public function index()
     {
         //
+        // $products = Owner::findOrFail(Auth::id())->shop->product;
+
+        // Eager(積極的) Loading で書き直します
+        $ownerInfo = Owner::with('shop.product.imageFirst') 
+            ->where('id', Auth::id())->get();
+        // dd($ownerInfo);
+
+        // foreach($ownerInfo as $owner) 
+        //     foreach($owner->shop->product as $product) {
+        //         dd($product->imageFirst->filename); 
+        //     }
+        // }
+
+        return view('owner.products.index', compact('ownerInfo'));
     }
 
     /**
