9e2fe6c47d71b6a1a5de90efc328796ec502b48f
ibumaxnetg <ibumaxnet@gmail.com>
2021-12-03 16:01:57 +0900

    ショップ情報の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 use App\Models\Product;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -49,7 +50,17 @@ class ItemController extends Controller
     public function show($id){
         $product = Product::findOrFail($id);
 
-        return view('user.show', compact('product'));
+        $quantity = Stock::where('product_id', $product->id)
+        ->sum('quantity');
+
+        $quantity = Stock::where('product_id', $product->id)
+                            ->sum('quantity');
+
+        if($quantity > 9) {
+            $quantity = 9;
+        }
+
+        return view('user.show', compact('product', 'quantity'));
       }
 
 }
